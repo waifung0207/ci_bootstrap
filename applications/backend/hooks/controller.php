b0d3ce25a02a85d9accb6cd9a63988195ab4a741
Michael Chan <waifung0207@gmail.com>
2014-04-29 22:23:33 +0800

    [Backend] Added features to generate menu items by mViewData['menu']

--- applications/backend/hooks/controller.php
+++ applications/backend/hooks/controller.php
@@ -2,38 +2,70 @@
 
 class Controller extends CI_Hooks
 {
-    // set default values
-    function setup()
-    {
-        $CI =& get_instance();
+	// set default values
+	function setup()
+	{
+		$CI =& get_instance();
 
-        $CI->mCtrler = $CI->router->fetch_class();
-        $CI->mAction = $CI->router->fetch_method();
-        $CI->mParam = $CI->uri->segment(3);
+		$CI->mCtrler = $CI->router->fetch_class();
+		$CI->mAction = $CI->router->fetch_method();
+		$CI->mParam = $CI->uri->segment(3);
 
-        // load language files
-        $CI->mLocale = $CI->config->item('language');
-        
-        // check if user is logged in
-        $user = $CI->session->userdata('user');
-        if ( empty($user) && $CI->mCtrler!='login' )
-        {
-            redirect('login');
-            return;
-        }
-        
-        // default values for page output
-        $CI->mLayout = "default";
+		// load language files
+		$CI->mLocale = $CI->config->item('language');
+		
+		// check if user is logged in
+		$user = $CI->session->userdata('user');
+		if ( empty($user) && $CI->mCtrler!='login' )
+		{
+			redirect('login');
+			return;
+		}
+		
+		// default values for page output
+		$CI->mLayout = "default";
 
-        // default theme
-        $CI->mTheme = DEFAULT_THEME;
-        
-        // prepare view data
-        $CI->mViewData = array(
-            'locale'    => $CI->mLocale,
-            'ctrler'    => $CI->mCtrler,
-            'action'    => $CI->mAction,
-            'user'      => $user
-        );
-    }
+		// default theme
+		$CI->mTheme = DEFAULT_THEME;
+		
+		// prepare view data
+		$CI->mViewData = array(
+			'locale'    => $CI->mLocale,
+			'ctrler'    => $CI->mCtrler,
+			'action'    => $CI->mAction,
+			'user'      => $user,
+			'menu'      => $this->setup_menu(),
+		);
+	}
+
+	// setup menu items
+	function setup_menu()
+	{
+		return array(
+			'home' => array(
+				'name'      => 'Home',
+				'url'       => site_url(),
+				'icon'      => 'fa fa-dashboard'
+			),
+
+			// Example to add sections with subpages
+			'manage' => array(
+				'name'      => 'Manage',
+				'url'       => site_url('manage'),
+				'icon'      => 'fa fa-cog',
+				'children'  => array(
+					'Users'			=> '#',
+					'News'			=> '#',
+					'Products'		=> '#',
+				)
+			),
+			// end of example
+
+			'logout' => array(
+				'name'      => 'Sign out',
+				'url'       => site_url('login/logout'),
+				'icon'      => 'fa fa-sign-out'
+			),
+		);
+	}
 }
--- applications/backend/views/_partial/menu.php
+++ applications/backend/views/_partial/menu.php
@@ -1,13 +1,41 @@
 
 <ul class="sidebar-menu">
-	<li>
-		<a href="<?php echo site_url(); ?>">
-			<i class="fa fa-dashboard"></i> <span>Home</span>
-		</a>
-	</li>
-	<li>
-		<a href="<?php echo site_url('login/logout'); ?>">
-			<i class="fa fa-sign-out"></i> <span>Sign out</span>
-		</a>
-	</li>
+	<?php
+		foreach ($menu as $parent => $parent_params)
+		{
+			$name = $parent_params['name'];
+			$url = $parent_params['url'];
+			$icon = $parent_params['icon'];
+
+			if ( empty($parent_params['children']) )
+			{
+				$active = ( current_url()==$url ) ? 'active' : '';
+				echo "<li class='$active'><a href='$url'><i class='$icon'></i> <span>$name</span></a></li>";
+			}
+			else
+			{
+				$parent_active = ( $ctrler==$parent ) ? 'active' : '';
+				$parent_name = $parent_params['name'];
+				$parent_url = $parent_params['url'];
+				$parent_icon = $parent_params['icon'];
+
+				// parent of child items
+				$list_arrow = ($parent_active) ? 'fa-angle-down' : 'fa-angle-left';
+				echo "<li class='treeview $parent_active'>
+					<a href='#'>
+						<i class='$parent_icon'></i> <span>$parent_name</span><i class='fa pull-right $list_arrow'></i>
+					</a>";
+
+				// child items
+				$list_display = ($parent_active) ? '' : 'display: none;';
+				echo "<ul class='treeview-menu' style='$list_display'>";
+				foreach ($parent_params['children'] as $name => $url)
+				{
+					echo "<li><a href='$url' style='margin-left: 10px;'><i class='fa fa-angle-double-right'></i> $name</a></li>";
+				}
+				echo "</ul>";
+				echo "</li>";
+			}
+		}
+	?>
 </ul>
