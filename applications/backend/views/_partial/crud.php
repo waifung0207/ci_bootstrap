7d342d72a9a0228852b3370af62c3b455d540995
Michael Chan <waifung0207@gmail.com>
2014-06-23 23:23:10 +0800

    Includes crud_note and back_url into crud view; added MY_Model from CI Base Model

--- applications/backend/views/_partial/crud.php
+++ applications/backend/views/_partial/crud.php
@@ -1,7 +1,10 @@
 
-<?php
-	if ( !empty($crud_data) )
-	{
-		echo $crud_data->output;
-	}
-?>
+<?php if ( !empty($crud_note) ) echo $crud_note; ?>
+
+<?php if ( !empty($crud_data) ) echo $crud_data->output; ?>
+
+<?php if ( !empty($back_url) ) { ?>
+	<a href="<?php echo $back_url; ?>" class="btn btn-primary">
+		<i class="fa fa-reply"></i> Back
+	</a>
+<?php } ?>
