38bc954c5cd02502b509512a11d1f9172a10ab6d
Michael Chan <waifung0207@gmail.com>
2015-02-28 19:32:47 +0800

    Minor update

--- applications/backend/views/404.php
+++ applications/backend/views/404.php
@@ -4,7 +4,7 @@
 		<h3><i class="fa fa-warning text-yellow"></i> Oops! Page not found.</h3>
 		<p>
 			We could not find the page you were looking for.
-			Meanwhile, you may <a href='<?php echo site_url(); ?>'>return to dashboard</a> or try using the search form.
+			Meanwhile, you may <a href='<?php echo site_url(); ?>'>return to dashboard</a>.
 		</p>
 
 		<?php /*
