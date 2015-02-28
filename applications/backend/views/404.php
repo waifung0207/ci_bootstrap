4de365417015a88ac078e73296c03e4d4a685ae0
Michael Chan <waifung0207@gmail.com>
2015-02-28 19:31:38 +0800

    Updated Backend 404 error page

--- applications/backend/views/404.php
+++ applications/backend/views/404.php
@@ -1,21 +1,21 @@
-
 <div class="error-page">
-	<h2 class="headline text-info"> 404</h2>
+	<h2 class="headline text-yellow"> 404</h2>
 	<div class="error-content">
 		<h3><i class="fa fa-warning text-yellow"></i> Oops! Page not found.</h3>
 		<p>
-			We could not find the page you were looking for.<br/>
-			Meanwhile, you may <a href="<?php echo site_url(); ?>">return to dashboard</a>.
+			We could not find the page you were looking for.
+			Meanwhile, you may <a href='<?php echo site_url(); ?>'>return to dashboard</a> or try using the search form.
 		</p>
-		<!--
-		<form class="search-form">
-			<div class="input-group">
-				<input type="text" name="search" class="form-control" placeholder="Search">
+
+		<?php /*
+		<form class='search-form'>
+			<div class='input-group'>
+				<input type="text" name="search" class='form-control' placeholder="Search"/>
 				<div class="input-group-btn">
-					<button type="submit" name="submit" class="btn btn-primary"><i class="fa fa-search"></i></button>
+					<button type="submit" name="submit" class="btn btn-warning btn-flat"><i class="fa fa-search"></i></button>
 				</div>
 			</div>
-		</form>
-		-->
+		</form> */
+		?>
 	</div>
 </div>
